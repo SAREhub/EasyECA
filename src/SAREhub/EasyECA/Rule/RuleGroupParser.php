b0987c7a1fcfdc82faaec5be0d0c62771a795f30
a.wasiak@sare.pl <a.wasiak@sare.pl>
2018-05-21 16:01:07 +0200

    added RuleGroupParser

---
+++ src/SAREhub/EasyECA/Rule/RuleGroupParser.php
@@ -0,0 +1,30 @@
+<?php
+
+
+namespace SAREhub\EasyECA\Rule;
+
+
+use SAREhub\Client\Processor\MulticastProcessor;
+use SAREhub\Client\Processor\Processors;
+
+class RuleGroupParser
+{
+    /**
+     * @var RuleParser
+     */
+    private $ruleParser;
+
+    public function __construct(RuleParser $ruleParser)
+    {
+        $this->ruleParser = $ruleParser;
+    }
+
+    public function parse(RuleGroupDefinition $definition): MulticastProcessor
+    {
+        $processor = Processors::multicast();
+        foreach ($definition->getRules() as $rule) {
+            $processor->add($this->ruleParser->parse($rule));
+        }
+        return $processor;
+    }
+}
---
+++ tests/unit/SAREhub/EasyECA/Rule/RuleGroupParserTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace SAREhub\EasyECA\Rule;
+
+use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
+use Mockery\MockInterface;
+use PHPUnit\Framework\TestCase;
+
+class RuleGroupParserTest extends TestCase
+{
+    use MockeryPHPUnitIntegration;
+    /**
+     * @var MockInterface | RuleParser
+     */
+    private $ruleParser;
+
+    /**
+     * @var RuleGroupParser
+     */
+    private $parser;
+
+    protected function setUp()
+    {
+        $this->ruleParser = \Mockery::mock(RuleParser::class);
+        $this->parser = new RuleGroupParser($this->ruleParser);
+    }
+
+    public function testParse()
+    {
+        $ruleDefinition = new RuleDefinition("");
+        $groupDefinition = new RuleGroupDefinition("test_group", [$ruleDefinition]);
+
+        $ruleProcessor = \Mockery::mock(CheckRuleProcessor::class);
+        $this->ruleParser->expects("parse")->withArgs([$ruleDefinition])->andReturn($ruleProcessor);
+        $processor = $this->parser->parse($groupDefinition);
+        $this->assertSame($ruleProcessor, $processor->getProcessors()[0]);
+    }
+}
