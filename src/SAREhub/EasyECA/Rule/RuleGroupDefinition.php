3ab02ab25fd57473b3c6d4fbb9eb81c762efcc0e
a.wasiak@sare.pl <a.wasiak@sare.pl>
2018-05-21 15:56:44 +0200

    added RuleGroupDefinition concept

---
+++ src/SAREhub/EasyECA/Rule/RuleGroupDefinition.php
@@ -0,0 +1,56 @@
+<?php
+
+
+namespace SAREhub\EasyECA\Rule;
+
+
+class RuleGroupDefinition implements \JsonSerializable
+{
+    /**
+     * @var string
+     */
+    private $id;
+
+    /**
+     * @var RuleDefinition[]
+     */
+    private $rules;
+
+    /**
+     * @param string $id
+     * @param RuleDefinition[] $rules
+     */
+    public function __construct(string $id, array $rules)
+    {
+        $this->id = $id;
+        $this->rules = $rules;
+    }
+
+    public function getId(): string
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return RuleDefinition[]
+     */
+    public function getRules(): array
+    {
+        return $this->rules;
+    }
+
+    public function jsonSerialize()
+    {
+        return $this->toArray();
+    }
+
+    public function toArray()
+    {
+        return [
+            'id' => $this->getId(),
+            'rules' => $this->getRules()
+        ];
+    }
+
+
+}
---
+++ src/SAREhub/EasyECA/Rule/RuleGroupDefinitionFactory.php
@@ -0,0 +1,51 @@
+<?php
+
+
+namespace SAREhub\EasyECA\Rule;
+
+
+class RuleGroupDefinitionFactory
+{
+    /**
+     * @var RuleDefinitionFactory
+     */
+    private $ruleDefinitionFactory;
+
+    public function __construct(RuleDefinitionFactory $ruleDefinitionFactory)
+    {
+        $this->ruleDefinitionFactory = $ruleDefinitionFactory;
+    }
+
+    /**
+     * @param array $data
+     * @return RuleGroupDefinition
+     */
+    public function create(array $data): RuleGroupDefinition
+    {
+        $this->assertData($data);
+
+        $rules = [];
+        foreach ($data["rules"] as $ruleData) {
+            $rules[] = $this->ruleDefinitionFactory->create($ruleData);
+        }
+        return new RuleGroupDefinition($data["id"], $rules);
+    }
+
+    private function assertData(array $data): void
+    {
+        if (empty($data["id"])) {
+            $this->throwInvalidDataException("empty id");
+        }
+        if (empty($data["rules"])) {
+            $this->throwInvalidDataException("empty rules");
+        }
+        if (!is_array($data["rules"])) {
+            $this->throwInvalidDataException("rules isn't array");
+        }
+    }
+
+    private function throwInvalidDataException(string $message)
+    {
+        throw new \InvalidArgumentException("RuleGroupDefinition data is invalid: $message");
+    }
+}
